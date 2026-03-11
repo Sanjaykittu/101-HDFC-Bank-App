30c2adb7f60cea83579cc48eb69eb9faef0dd7fb
Sanjay Kittu <kittuss1119@gmail.com>
2026-03-11 22:04:34 +0530

    som

--- Demo.java
+++ Demo.java
@@ -3,16 +3,10 @@ public class Demo{
 //jera 101
 public void m1(){
 	//logic
-} 
-=======
-public class{
-  public void m1(){
-    //logic
-  }  
-  int a=10;
-  int b=20;
 
+}
+int a=10;
+int b= 20;
 
->>>>>>> 1bcded9acc7460e719f17cb110f0daae5677f176
 }
  	
